fafdf0bb4d8f59f361aa644bdabd11b0ff10e4a0
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-01-11 16:14:54 -0800

    introduce version pinning. Rebase from PR #651 from stripe/mickjermsurawong/version-pin-java
    make alphabetically order
    
    rename to override instead of onBehalf of

--- src/main/java/com/stripe/net/RequestOptions.java
+++ src/main/java/com/stripe/net/RequestOptions.java
@@ -11,27 +11,29 @@ public class RequestOptions {
 
   private final String apiKey;
   private final String clientId;
+  private final String idempotencyKey;
+  private final String stripeAccount;
   /**
    * Stripe version always set at {@link Stripe#API_VERSION}.
    */
   private final String stripeVersion = Stripe.API_VERSION;
   /**
-   * Stripe version when made on behalf of others. This can be used when the returned response
-   * will not be deserialized into the current classes pinned to {@link Stripe#VERSION}.
+   * Stripe version override when made on behalf of others. This can be used when the returned
+   * response will not be deserialized into the current classes pinned to {@link Stripe#VERSION}.
    */
-  private final String stripeVersionOnBehalfOf;
-  private final String idempotencyKey;
-  private final String stripeAccount;
+  private final String stripeVersionOverride;
+
   private final int connectTimeout;
   private final int readTimeout;
 
-  private RequestOptions(String apiKey, String clientId, String stripeVersionOnBehalfOf,
-      String idempotencyKey, String stripeAccount, int connectTimeout, int readTimeout) {
+  private RequestOptions(String apiKey, String clientId, String idempotencyKey,
+                         String stripeAccount, String stripeVersionOverride,
+                         int connectTimeout, int readTimeout) {
     this.apiKey = apiKey;
     this.clientId = clientId;
-    this.stripeVersionOnBehalfOf = stripeVersionOnBehalfOf;
     this.idempotencyKey = idempotencyKey;
     this.stripeAccount = stripeAccount;
+    this.stripeVersionOverride = stripeVersionOverride;
     this.connectTimeout = connectTimeout;
     this.readTimeout = readTimeout;
   }
@@ -44,20 +46,20 @@ public class RequestOptions {
     return clientId;
   }
 
-  public String getStripeVersionOnBehalfOf() {
-    return stripeVersionOnBehalfOf;
-  }
-
-  public String getStripeVersion() {
-    return stripeVersion;
-  }
-
   public String getIdempotencyKey() {
     return idempotencyKey;
   }
 
   public String getStripeAccount() {
     return stripeAccount;
+  }
+
+  public String getStripeVersion() {
+    return stripeVersion;
+  }
+
+  public String getStripeVersionOverride() {
+    return stripeVersionOverride;
   }
 
   public int getReadTimeout() {
@@ -85,12 +87,21 @@ public class RequestOptions {
     if (clientId != null ? !clientId.equals(that.clientId) : that.clientId != null) {
       return false;
     }
-    if (idempotencyKey != null ? !idempotencyKey.equals(that.idempotencyKey)
-        : that.idempotencyKey != null) {
+    if (idempotencyKey != null ? !idempotencyKey.equals(that.idempotencyKey) :
+        that.idempotencyKey != null) {
       return false;
     }
-    if (stripeVersion != null ? !stripeVersion.equals(that.stripeVersion)
-        : that.stripeVersion != null) {
+    if (stripeAccount != null ? !stripeAccount.equals(that.stripeAccount) :
+        that.stripeAccount != null) {
+      return false;
+    }
+    if (stripeVersion != null ? !stripeVersion.equals(that.stripeVersion) :
+        that.stripeVersion != null) {
+      return false;
+    }
+    if (stripeVersionOverride != null
+        ? !stripeVersionOverride.equals(that.stripeVersionOverride) :
+        that.stripeVersionOverride != null) {
       return false;
     }
 
@@ -105,11 +116,11 @@ public class RequestOptions {
   public int hashCode() {
     int result = apiKey != null ? apiKey.hashCode() : 0;
     result = 31 * result + (clientId != null ? clientId.hashCode() : 0);
-    result = 31 * result + (stripeVersion != null ? stripeVersion.hashCode() : 0);
-    result = 31 * result + (stripeVersionOnBehalfOf != null ? stripeVersionOnBehalfOf.hashCode()
-        : 0);
     result = 31 * result + (idempotencyKey != null ? idempotencyKey.hashCode() : 0);
     result = 31 * result + (stripeAccount != null ? stripeAccount.hashCode() : 0);
+    result = 31 * result + (stripeVersion != null ? stripeVersion.hashCode() : 0);
+    result = 31 * result + (stripeVersionOverride != null ? stripeVersionOverride.hashCode()
+        : 0);
     result = 31 * result + connectTimeout;
     result = 31 * result + readTimeout;
     return result;
@@ -119,19 +130,21 @@ public class RequestOptions {
     return new RequestOptionsBuilder();
   }
 
+  /**
+   * Convert request options to builder, retaining invariant values for the integration.
+   * @return option builder.
+   */
   public RequestOptionsBuilder toBuilder() {
     return new RequestOptionsBuilder()
-        .setApiKey(this.apiKey)
-        .setStripeVersionOnBehalfOf(this.stripeVersionOnBehalfOf)
-        .setStripeAccount(this.stripeAccount);
+        .setApiKey(this.apiKey).setStripeAccount(this.stripeAccount);
   }
 
   public static final class RequestOptionsBuilder {
     private String apiKey;
     private String clientId;
-    private String stripeVersionOnBehalfOf;
     private String idempotencyKey;
     private String stripeAccount;
+    private String stripeVersionOverride;
     private int connectTimeout;
     private int readTimeout;
 
@@ -169,23 +182,6 @@ public class RequestOptions {
 
     public RequestOptionsBuilder clearClientId() {
       this.clientId = null;
-      return this;
-    }
-
-    /**
-     * Do not use this except for in API where JSON response is not fully deserialize into
-     * to explicit Stripe resources -- it intends to make request on behalf of others, makes
-     * request with their API version, and simply passes the raw values along. Example for this is
-     * {@link com.stripe.model.EphemeralKey#create(Map, RequestOptions)}.
-     *
-     * Setting this value in typical scenario will result in deserialization error
-     * as the model classes has schema according to the pinned {@link Stripe#API_VERSION} and not
-     * the {@code stripeVersionOnBehalfOf}
-     * @param stripeVersionOnBehalfOf stripe version of the client to make request on behalf of
-     * @return option builder
-     */
-    public RequestOptionsBuilder setStripeVersionOnBehalfOf(String stripeVersionOnBehalfOf) {
-      this.stripeVersionOnBehalfOf = normalizeStripeVersion(stripeVersionOnBehalfOf);
       return this;
     }
 
@@ -250,6 +246,31 @@ public class RequestOptions {
       return setStripeAccount(null);
     }
 
+    public String getStripeVersionOverride() {
+      return this.stripeVersionOverride;
+    }
+
+    /**
+     * Do not use this except for in API where JSON response is not fully deserialized into
+     * to explicit Stripe classes, but only passed to other clients as raw data -- essentially
+     * making request on behalf of others with their API version. One example is in
+     * {@link com.stripe.model.EphemeralKey#create(Map, RequestOptions)}.
+     * Setting this value in a typical scenario will result in deserialization error
+     * as the model classes have schema according to the pinned {@link Stripe#API_VERSION} and not
+     * the {@code stripeVersionOverride}
+     * @param stripeVersionOverride stripe version override which belongs to the client to make
+     *                              request on behalf of.
+     * @return option builder
+     */
+    public RequestOptionsBuilder setStripeVersionOverride(String stripeVersionOverride) {
+      this.stripeVersionOverride = normalizeStripeVersion(stripeVersionOverride);
+      return this;
+    }
+
+    public RequestOptionsBuilder clearStripeVersionOverride() {
+      return setStripeVersionOverride(null);
+    }
+
     /**
      * Constructs a {@link RequestOptions} with the specified values.
      */
@@ -257,9 +278,9 @@ public class RequestOptions {
       return new RequestOptions(
           normalizeApiKey(this.apiKey),
           normalizeClientId(this.clientId),
-          normalizeStripeVersion(this.stripeVersionOnBehalfOf),
           normalizeIdempotencyKey(this.idempotencyKey),
           normalizeStripeAccount(this.stripeAccount),
+          normalizeStripeVersion(this.stripeVersionOverride),
           connectTimeout,
           readTimeout);
     }
--- src/test/java/com/stripe/functional/EphemeralKeyTest.java
+++ src/test/java/com/stripe/functional/EphemeralKeyTest.java
@@ -13,8 +13,6 @@ import com.stripe.net.RequestOptions;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class EphemeralKeyTest extends BaseStripeTest {
@@ -27,7 +25,7 @@ public class EphemeralKeyTest extends BaseStripeTest {
     params.put("customer", "cus_123");
 
     final RequestOptions options = RequestOptions.builder()
-        .setStripeVersionOnBehalfOf("2017-05-25").build();
+        .setStripeVersionOverride("2017-05-25").build();
 
     final EphemeralKey key = EphemeralKey.create(params, options);
 
@@ -42,14 +40,14 @@ public class EphemeralKeyTest extends BaseStripeTest {
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void testCreateWithoutApiVersionOnBehalf() throws StripeException {
+  public void testCreateWithoutApiVersionOverride() throws StripeException {
     Stripe.apiVersion = null;
 
     final Map<String, Object> params = new HashMap<>();
     params.put("customer", "cus_123");
 
     final RequestOptions options = RequestOptions.getDefault();
-    assertNull(options.getStripeVersionOnBehalfOf());
+    assertNull(options.getStripeVersionOverride());
 
     EphemeralKey.create(params, options);
   }
@@ -59,7 +57,8 @@ public class EphemeralKeyTest extends BaseStripeTest {
     final Map<String, Object> params = new HashMap<>();
     params.put("customer", "cus_123");
 
-    final RequestOptions options = RequestOptions.builder().setStripeVersionOnBehalfOf("2017-05-25").build();
+    final RequestOptions options = RequestOptions.builder()
+        .setStripeVersionOverride("2017-05-25").build();
 
     final EphemeralKey key = EphemeralKey.create(params, options);
 
